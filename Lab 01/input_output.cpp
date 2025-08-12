95670e09e109fe682dbd44519c7f2bbafe432645
Sourav Saha <souravsahapartho@gmail.com>
2025-08-12 19:12:33 +0600

    Create input_output.cpp

---
+++ Lab 01/input_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello world!!!" << endl ;
+    cout << "Welcome to DSA-2 Lab" << endl;
+    int a, b, sum;
+    cout << "Enter a: ";
+    cin >> a;
+    cout << "Enter b:";
+    cin >> b;
+    sum = a + b;
+    cout << "sum : " << sum << " ." << endl;
+    int c, d;
+    cout << "Enter c, d :";
+    cin >> c >> d ;
+    sum = c + d;
+    cout << "sum of c, d : " << sum << endl;
+}
+
